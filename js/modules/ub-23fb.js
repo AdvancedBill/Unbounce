c97fd51c4b680b1c8b49e84c0e3b4b3518498d95
AdvancedBill <119365200+AdvancedBill@users.noreply.github.com>
2023-03-29 10:29:42 -0600

    comments ub-23fb.js

--- js/modules/ub-23fb.js
+++ js/modules/ub-23fb.js
@@ -1,11 +1,13 @@
 // ub-23fb.js
 import { promoHandler } from "./promoHandler.js";
 
+// searches head for git version
 const scriptTag = document.querySelector(
   'script[type="module"][src*="/js/modules/ub-23fb.js"]'
 );
+// matches version number and passes to promoHandler
 const src = scriptTag.getAttribute("src");
 const versionMatch = src.match(/AdvancedBill\/Unbounce@([a-f0-9]+)\//);
 const versionNo = versionMatch ? versionMatch[1] : null;
-console.log(versionNo);
+// console.log(versionNo);
 promoHandler(versionNo);

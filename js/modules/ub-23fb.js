5430dd6b0b15599e7fb24d98b0e10e5f9d01b3cc
AdvancedBill <119365200+AdvancedBill@users.noreply.github.com>
2023-03-29 10:14:17 -0600

    updates code to dynamically set the versionNo

--- js/modules/ub-23fb.js
+++ js/modules/ub-23fb.js
@@ -1,5 +1,11 @@
 // ub-23fb.js
 import { promoHandler } from "./promoHandler.js";
 
-const versionNo = "d394673258d4fee82e7a40595c55ed5b63737440";
+const scriptTag = document.querySelector(
+  'script[type="module"][src*="/js/modules/ub-23fb.js"]'
+);
+const src = scriptTag.getAttribute("src");
+const versionMatch = src.match(/AdvancedBill\/Unbounce@([a-f0-9]+)\//);
+const versionNo = versionMatch ? versionMatch[1] : null;
+console.log(versionNo);
 promoHandler(versionNo);
